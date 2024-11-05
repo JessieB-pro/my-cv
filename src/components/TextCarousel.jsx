8c676f80d5f770197d2fe7ca360989bb3ef6905f
Jess <jess@monadresse.com>
2024-11-05 15:52:33 +0100

    rajout d'une base de données Centres d'intérêt via Firebase et Cloud Firestore

--- src/components/TextCarousel.jsx
+++ src/components/TextCarousel.jsx
@@ -1,9 +1,49 @@
 import { Carousel } from 'react-bootstrap';
 
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './FirebaseConfig';
+
 function TextCarousel() {
+
+    const [centresInteret, setCentresInteret] = useState();
+
+    const getCentresInteret = async () => {
+        const querySnapshot = await getDocs(collection(db, "centresInteret"));
+
+        const centresInteret = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+
+        // querySnapshot.forEach((doc) => {
+        //     console.log(doc.id, " => ", doc.data());
+        // });
+
+        setCentresInteret(centresInteret)
+    }
+
+    useEffect(() => {
+        getCentresInteret()
+    }, []);
+
     return (
         <Carousel id="myCarousel">
-            <Carousel.Item interval={8000}>
+            {centresInteret ? (
+                centresInteret.map((centreInteret, i) => (
+                    <Carousel.Item interval={8000}>
+                        <div id="carousel-item" className="justify-content-center align-items-center" key={centreInteret.id}>
+                            <h3 className='mb-5'>{centreInteret.title}</h3>
+                            <h4 className='mb-5'>{centreInteret.citation}</h4>
+                        </div>
+
+                    </Carousel.Item>
+                ))
+            ) : (
+                <div className="justify-content-center align-items-center">
+                    <h4>Oups, mes centres d'intérêt se sont échappés...</h4>
+                </div>
+            )
+            }
+
+            {/* <Carousel.Item interval={8000}>   
                 <div id="carousel-item" className="justify-content-center align-items-center">
                     <h3 className='mb-5'>Le Chant et la Guitare pour respirer</h3>
                     <h4 className='mb-5'>"La musique pour moi, c'est comme respirer. Je ne me lasse pas de respirer, je ne me lasse pas de la musique" de Ray Charles</h4>
@@ -20,7 +60,7 @@ function TextCarousel() {
                     <h3 className='mb-5'>La Lecture pour découvrir</h3>
                     <h4 className='mb-5'>"Explorer de nouveaux endroits est un excellent moyen de se détendre et de se vider l'esprit"  de Holly R. Patrick</h4>
                 </div>
-            </Carousel.Item>
+            </Carousel.Item> */}
         </Carousel>
     );
 }
