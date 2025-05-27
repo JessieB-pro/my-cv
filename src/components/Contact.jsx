24d031057cbb9805886157beb4cfbf53bd7fee26
Jess <jess@monadresse.com>
2025-05-27 12:30:34 +0200

    attempt to solve a problem : button Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,31 @@
 import linkedin from '../assets/linkedin.png';
+import { useEffect } from 'react';
 
 export default function Contact() {
+
+
+useEffect(() => {
+    const links = document.querySelectorAll('a');
+
+    links.forEach(link => {
+      // Quand l'utilisateur clique sur un lien, on supprime le focus avec un délai court
+      link.addEventListener('click', handleBlurAfterClick);
+    });
+
+    function handleBlurAfterClick(e) {
+      setTimeout(() => {
+        e.currentTarget.blur();
+      }, 100); // délai pour laisser le lien s’ouvrir
+    }
+
+    // Nettoyage à la désactivation du composant
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', handleBlurAfterClick);
+      });
+    };
+  }, []);
+
   return (
     <>
       <div id="contact" className="container-fluid">
@@ -13,19 +38,12 @@ export default function Contact() {
 
           <div className="d-flex flex-wrap justify-content-center align-items-center">
 
-            <a
-              href="https://www.linkedin.com/in/jessie-bagneris-pro/"
-              className="button-contact-in"
-              target='_blank'
-              onMouseUp={(e) => e.currentTarget.blur()}>
+            <a href="https://www.linkedin.com/in/jessie-bagneris-pro/" className="button-contact-in" target='_blank'>
               <h5>Message</h5>
               <img src={linkedin} alt="icon linkedin" />
             </a>
 
-            <a
-              href="#contact"
-              className="button-contact-meet"
-              onMouseUp={(e) => e.currentTarget.blur()}>
+            <a href="#contact" className="button-contact-meet">
               <h5>Rencontre</h5>
               <p className='meet-style'>Disponible sur Toulouse et sa périphérie.</p>
             </a>
